c56aed2b6cc8ec2db671553844f0615bf2fb5749
jdw5 <87954417+jdw5@users.noreply.github.com>
2025-02-09 06:41:08 +0000

    Fix styling

--- tests/Pest/MiddlewareTest.php
+++ tests/Pest/MiddlewareTest.php
@@ -18,34 +18,34 @@ beforeEach(function () {
     ]);
 });
 
-it('shares the navigation', function () {    
-    get(route('products.index'))->assertInertia(fn (Assert $page) => $page
-        ->has(Nav::ShareProp, 2)
-        ->where('nav.0', [
-            'label' => 'Index',
-            'href' => route('products.index'),
-            'active' => true,
-        ])
-        ->where('nav.1', [
-            'label' => 'Show',
-            'href' => route('products.show', $this->product),
-            'active' => false,
-        ])
-    );
-});
+it('shares the navigation', function () {
+get(route('products.index'))->assertInertia(fn (Assert $page) => $page
+    ->has(Nav::ShareProp, 2)
+    ->where('nav.0', [
+        'label' => 'Index',
+        'href' => route('products.index'),
+        'active' => true,
+    ])
+    ->where('nav.1', [
+        'label' => 'Show',
+        'href' => route('products.show', $this->product),
+        'active' => false,
+    ])
+);
+    });
 
 it('can share multiple groups', function () {
-    get('/')->assertInertia(fn (Assert $page) => $page
-        ->has(Nav::ShareProp, 1)
-        ->where(Nav::ShareProp.'.sidebar.0', [
-            'label' => 'Index',
-            'href' => route('products.index'),
-            'active' => false,
-        ])
-        ->where(Nav::ShareProp.'.sidebar.1', [
-            'label' => 'Show',
-            'href' => route('products.show', $this->product),
-            'active' => false,
-        ])
-    );
-});
+get('/')->assertInertia(fn (Assert $page) => $page
+    ->has(Nav::ShareProp, 1)
+    ->where(Nav::ShareProp.'.sidebar.0', [
+        'label' => 'Index',
+        'href' => route('products.index'),
+        'active' => false,
+    ])
+    ->where(Nav::ShareProp.'.sidebar.1', [
+        'label' => 'Show',
+        'href' => route('products.show', $this->product),
+        'active' => false,
+    ])
+);
+    });
